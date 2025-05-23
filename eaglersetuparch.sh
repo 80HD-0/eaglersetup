4e5a4d6043c00693db6b02504135a768025ee0f0
80HD Studios <afunnierguy@gmail.com>
2025-05-22 22:41:08 -0700

    Upload most of 1.1
    added an arch version, fixed text, added code to install java on both systems
    NOT FINISHED 1.1 YET

--- eaglersetuparch.sh
+++ eaglersetuparch.sh
@@ -26,27 +26,24 @@ if [ -z "$folder" ]; then
 	folder=.
 fi
 echo "Eaglersetup is now downloading the server. This might take a bit."
+sudo pacman -S --noconfirm git > /dev/null 2>/dev/null
+sudo pacman -S --noconfirm jre-openjdk > /dev/null 2>/dev/null
 git clone https://github.com/Eaglercraft-Templates/Eaglercraft-Server-Paper "$folder"
 chmod +x ./$folder/run.sh
 echo "Done downloading the server!"
 if [ -n "$domain" ]; then
 	echo "Since you told us your domain, eaglersetup is now setting up WSS support with Apache2. Please note that this is not configured to work with NGINX. if you don't know what that is, you can ignore this message."
 	echo "this part might take a while, especially if you don't have apache2 installed (on ubuntu it's preinstalled)"
-	sudo apt install apache2 -y > /dev/null 2>/dev/null
-	sudo apt install certbot -y > /dev/null 2>/dev/null
-	apt install -y wget apt-transport-https gpg # this part is ripped from docs.eags.pw (no need to reinvent the wheel)
-	wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
-	echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
-	apt update
-	apt install temurin-17-jdk # rest is mine
-	sudo systemctl stop apache2 > /dev/null 2>/dev/null
+	sudo pacman -S --noconfirm apache > /dev/null 2>/dev/null
+	sudo pacman -S --noconfirm certbot > /dev/null 2>/dev/null
+	sudo systemctl stop httpd > /dev/null 2>/dev/null
 	sudo certbot certonly --standalone -d "$domain"
-	sudo systemctl start apache2 > /dev/null 2>/dev/null
+	sudo systemctl start httpd > /dev/null 2>/dev/null
 	read -p "Provide your intended eaglerxserver port. It will default to 25565, which is also the default for eaglerxserver. Leave it blank if you don't know what to do. # " port
 	if [ -z "$port" ] || [ "$port" -lt 1024 ] || [ "$port" -gt 65535 ]; then
 		port=25565
 	fi
-	sudo tee /etc/apache2/sites-available/$domain.conf > /dev/null <<EOF
+	sudo tee /etc/httpd/conf/extra/$domain.conf > /dev/null <<EOF
 	<VirtualHost *:443>
 	    ServerName $domain
 
@@ -67,9 +64,15 @@ if [ -n "$domain" ]; then
 	    CustomLog \${APACHE_LOG_DIR}/$domain-access.log combined
 	</VirtualHost>
 EOF
-	sudo a2enmod proxy proxy_http proxy_wstunnel rewrite ssl > /dev/null
-	sudo a2ensite "$domain.conf" > /dev/null
-	sudo systemctl restart apache2
+	sudo sed -i '/^#LoadModule proxy_module/s/^#//' /etc/httpd/conf/httpd.conf
+	sudo sed -i '/^#LoadModule proxy_http_module/s/^#//' /etc/httpd/conf/httpd.conf
+	sudo sed -i '/^#LoadModule proxy_wstunnel_module/s/^#//' /etc/httpd/conf/httpd.conf
+	sudo sed -i '/^#LoadModule rewrite_module/s/^#//' /etc/httpd/conf/httpd.conf
+	sudo sed -i '/^#LoadModule ssl_module/s/^#//' /etc/httpd/conf/httpd.conf
+	if ! grep -q "extra/$domain.conf" /etc/httpd/conf/httpd.conf; then
+    		echo "Include conf/extra/$domain.conf" | sudo tee -a /etc/httpd/conf/httpd.conf
+	fi
+	sudo systemctl restart httpd
 	sed -i 's/25565/$port/' $folder/server.properties
 fi
 read -p "Now do you want to run the server? [Y/n] # " yn
